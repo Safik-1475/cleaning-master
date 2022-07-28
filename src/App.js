b69fe20610b0e918e41724fe22c7a15e1d5abe00
Safi <safikurrahman7514@gmail.com>
2022-07-29 05:59:33 +0600

    cleaning master initialition

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl">Hello Tailwindcss </h1>
+      <button className="btn btn-primary">Hello</button>
+    </>
+  );
+}
+
+export default App;
