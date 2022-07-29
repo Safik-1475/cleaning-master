5bd5e5b8a630ddf319cc3d7aa020fd4bccbbe1f0
Safi <safikurrahman7514@gmail.com>
2022-07-29 06:57:52 +0600

    added navbar dynamic router dark theme and dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home/Home"
+import { publicRoutes } from "./Routes/PublicRoutes";
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl">Hello Tailwindcss </h1>
-      <button className="btn btn-primary">Hello</button>
+      <Navbar>
+        <Routes>
+          {
+            publicRoutes.map(({ path, Component }, index) => (<Route key={index} path={path} element={<Component />} />))
+          }
+        </Routes>
+      </Navbar>
     </>
   );
 }
